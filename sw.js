365117ca5e61d5b3de949eaacdfbe8e5ee004a50
kunal1234 <kunal.bendekar@gmail.com>
2020-02-12 21:49:18 +0530

    Updated sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener('push', () => {
-	self.registration.sendNotification("Test Message 1", {});
+	var options = {
+		body: "This is Body",
+		vibrate: [100, 50, 100]
+	}
+	e.waitUntil(self.registration.showNotification("Test Message 1", options));
 })
