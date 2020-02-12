a4f38f33926711c23f18882d42cd9fe3a5174bb6
kunal1234 <kunal.bendekar@gmail.com>
2020-02-12 21:45:25 +0530

    updated push.js

--- push.js
+++ push.js
@@ -7,5 +7,5 @@ var vapidKeys = {
 
 push.setVapidDetails('mailto:test@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/e8_28gjzbls:APA91bGso4yVoPllgGG0O9XXesWiD3WWYofl6R6DDxBNWQ6RPJFjOfnmujyYol6SqRTHHOtcjpmpuiVIYWVbE8GumouZznF0HA9XPrI4_XhVBsVQaBBb9OG9EtqPSvEuops-wP4ftsvb","expirationTime":null,"keys":{"p256dh":"BKF7ROX3qTxJ5NHMNCEkCu0XZL_0y0nX7eF-ZETgtEZ7cwCYABBTadD30DV8VJNS9TvspGFjpdoBDtVyoP-P-0o","auth":"SEjWPbBwUlF7TQAGEu18cA"}};
 push.sendNotification(sub, "Test Message")
