cd5eb2f7b74e8607f442648efa6407da7ab326f1
kunal1234 <kunal.bendekar@gmail.com>
2020-02-25 19:12:33 +0530

    Updated Push JS

--- push.js
+++ push.js
@@ -1,27 +1,104 @@
 var push = require('web-push');
 var express = require('express');
+var path = require("path");
 var app = express();
-//var rootCas = require('ssl-root-cas/latest').create();
- 
-//rootCas.addFile(__dirname + '/ssl/01-cheap-ssl-intermediary-a.pem').addFile(__dirname + '/ssl/02-cheap-ssl-intermediary-b.pem');
- 
-// will work with all https requests will all libraries (i.e. request.js)
-//require('https').globalAgent.options.ca = rootCas;
+var bodyParser = require("body-parser");
+const MongoClient = require('mongodb').MongoClient;
+app.use(express.static('./'))
+
 var port = process.env.PORT || 8080;
 
 
-app.get('/', function (req, res) {
-	var vapidKeys = {
-	  publicKey: 'BLb6aIeyzqzi58NGwfM5IjDw01bsJEbdQM4jHSjnYs83uEURDKR27Pw4TtlSelvEGzkVdyIMlU3RVd2KVYSAgcg',
-	  privateKey: '0DC1Cgn1KN_fC9yHIFO4ePwA-7PpLIn95jfxYv-Fv60'
-	}
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-	push.setVapidDetails('mailto:test@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-	let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cZhxP0tu2oU:APA91bGp_4L3LlHHCmBXaNsJbJIX6AE3f95acX7ovurD6jlwRtDT_mi3k5dTL0C2ZYbhDCLu0vBKOgk_yb6dN0tCEBpRgx2dX1rRhvwVjNWuxVwXjGZWChmmBhBYoDR1KdR7CBr9cJFj","expirationTime":null,"keys":{"p256dh":"BImyrQx_3bSdaJ73aorASMF5LHSX3IskoVs8_hylgLYEsvpAlfbomCVkEwE1CjOz39TbhRJ29u2WLoHBlvQ570w","auth":"VjfZmB22jPPn81R6sQTsRw"}};
-	push.sendNotification(sub, "Test Message");
-	res.send('Notification Sent!');
+/*const uri = "mongodb+srv://admin-user:admin-user@webpushdb-6hegz.gcp.mongodb.net/test?retryWrites=true&w=majority";
+MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true}, (err, clien) => {
+  const collection = client.db("web-push-db").collection("users");
+  // perform actions on the collection object
+  console.log("connected");
+  var ins = {
+				subscriber : 'Test',
+				is_dinner : false,
+				is_time : false
+			} 
+  collection.insertOne(ins, function(err, res){
+	  console.log(err);
+  })
+  client.close();
 });
+*/
+const uri = "mongodb+srv://admin-user:admin-user@webpushdb-6hegz.gcp.mongodb.net/test?retryWrites=true&w=majority";
+
+
+app.post('/subscriber', function(req, res){
+		(async function() {
+		  await insertSubscriber(req.body);
+		  res.end("yes");
+		})();
+});
+
+
+app.get('/send-notification', function (req, res) {
+		
+	(async function() {
+		var subscribers = await getAllSubscriber();
+	  
+		var vapidKeys = {
+		  publicKey: 'BLb6aIeyzqzi58NGwfM5IjDw01bsJEbdQM4jHSjnYs83uEURDKR27Pw4TtlSelvEGzkVdyIMlU3RVd2KVYSAgcg',
+		  privateKey: '0DC1Cgn1KN_fC9yHIFO4ePwA-7PpLIn95jfxYv-Fv60'
+		}
+			
+		push.setVapidDetails('mailto:test@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
+	  
+	  for (const subscriber of subscribers) {
+		console.log(JSON.parse(subscriber.userId));
+		push.sendNotification(JSON.parse(subscriber.userId), "Would you like to have Dinner tonight !!");
+	  }
+	  
+	  res.send("Notification Sent!");
+	})();
+});
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
+
+//Insert document to db collection
+function insertSubscriber(myobj){
+	return new Promise(resolve => {
+		MongoClient.connect(uri,{ useUnifiedTopology: true }, function(err, database) {
+		  if (err) throw err;
+		  var databaseCollection = database.db("web-push-db");
+		  databaseCollection.collection("users").insertOne(myobj, function(err, res) {
+			if (err) throw err;
+			console.log("1 document inserted");
+			resolve();
+			database.close();
+		  });
+		});
+	});
+};
+
+//Get All document from db collection
+function getAllSubscriber(){
+	return new Promise(resolve => {
+		MongoClient.connect(uri,{ useUnifiedTopology: true }, function(err, database) {
+		  if (err) throw err;
+		  var databaseCollection = database.db("web-push-db");
+		  databaseCollection.collection("users").find({}).toArray(function(err, result) {
+			if (err) throw err;
+			resolve(result);
+			database.close();
+		  });
+		});
+	})
+}
+
 
 app.listen(port, function() {
     console.log('Our app is running  ' + port);
